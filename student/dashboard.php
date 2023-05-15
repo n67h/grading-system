ef8753d6a89afbc0dfa04b0063188210751bab4b
n67h <night67hunter@gmail.com>
2023-05-15 21:58:17 +0800

    Add student dashboard

---
+++ student/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+    // require_once 'includes/session.inc.php';
+    require_once '../connection/db.inc.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <?php
+        require_once 'sidebar.php';
+    ?>
+        <div class="container">
+            <div class="row mb-5 mt-5">
+        
+        </div>
+    <?php
+        require_once 'scripts.php';
+    ?>
